9ee665c5450702ff08a2a2b80ab2ffac986aa9c5
Jose Junior <jlfjunior8@gmail.com>
2025-03-24 20:43:12 -0300

    tests: add test projects structure

---
+++ tests/Opah.Consolidation.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace Opah.Consolidation.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/Opah.Transaction.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace Opah.Transaction.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
